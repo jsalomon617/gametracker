c0cce1a96c2ce460e3198a46962783568b7aa645
Jesse Salomon <jsalomon617@gmail.com>
2018-05-09 17:42:50 -0700

    clenaed up date stat logic, added 7 day rolling net

--- game_collection.py
+++ game_collection.py
@@ -14,7 +14,9 @@ class Event:
     GET = 1
     PLAY = 2
 
+
 class Game(object):
+    """Stores relevant info about a specific game"""
 
     def __init__(self, name):
         # store our initial info about the game
@@ -32,7 +34,57 @@ class Game(object):
         """Whether or not we've played the game yet"""
         return self.play is not None
 
+
+class Date(object):
+    """Stores relevant info about a particular date"""
+
+    def __init__(self, date):
+        ### store the datetime date object
+        self.date = date
+
+        ### store other things we care about for later
+
+        # list of (game, event) pairs that took place on this day
+        self.events = []
+
+        # net change in games today
+        self.net = 0
+
+        # did we acquire a game today?
+        self.acquired = False
+
+        # various stats we'll collate in the collection
+        self.stats = {
+            # count of total games owned on this date
+            "count": 0,
+
+            # net change in games in the last 7 days
+            "net_week": 0,
+        }
+
+    def record_event(self, game, event):
+        """Given a game and an event, record that they happened on this date"""
+
+        # just append both things to our internal list
+        self.events.append((game, event))
+
+        # then modify our net
+        if event == Event.GET:
+            self.net += 1
+            self.acquired = True
+        elif event == Event.PLAY:
+            self.net -= 1
+
+    def games_get(self):
+        """List the games we received today"""
+        return [g for (g,e) in self.events if e == Event.GET]
+
+    def games_play(self):
+        return [g for (g,e) in self.events if e == Event.PLAY]
+
+
 class Collection(object):
+    """Stores and compiles all of the relevant info about the collection"""
 
     DATA = "data.txt"
 
@@ -74,18 +126,21 @@ class Collection(object):
 
     def wipe(self):
         """Wipe our stored data (also useful for initializing)"""
-        
-        # for each date, store the list of (game, event) pairs
-        self.datestore = defaultdict(list)
 
-        # also for each date, store how many games we had on that day
-        self.datecounts = defaultdict(int)
-
-        # for each game, store its info
+        # for each game, store its object (keyed by name)
         self.gamestore = {}
+
+        # for each date, store its object (keyed by datetime.date object)
+        self.datestore = {}
 
         # keep track of the last day on which we acquired a game
         self.last_acquired = None
+
+    def get_date(self, date):
+        """Given a specific date, get it from datestore (creating it if necessary)"""
+        if date not in self.datestore:
+            self.datestore[date] = Date(date)
+        return self.datestore[date]
 
     def store(self):
         """Store the dataset by date and by name"""
@@ -106,9 +161,6 @@ class Collection(object):
                 self.gamestore[name] = Game(name)
             gameobj = self.gamestore[name]
 
-            # add the game and event to our datestore
-            self.datestore[date].append((gameobj, event))
-
             # modify the game object with our event
             if event == Event.GET:
                 gameobj.get = date
@@ -119,40 +171,60 @@ class Collection(object):
                     "game '{name}' has invalid event: '{event}'".format(
                         name=name, event=event))
 
-        # get the count each day from START to TODAY
+            # add the game and event to our date object
+            self.get_date(date).record_event(gameobj, event)
+
+
+        # get the total count (and other fun stats) each day from START to TODAY
+        # (most stats rely on previous days already having count defined)
         current = START
         while current <= TODAY:
-            # sum up our game events
-            counter = 0
-            for (_, event) in self.datestore[current]:
-                if event == Event.GET:
-                    counter += 1
-                    # we acquired a game on this day
-                    self.last_acquired = current
-                elif event == Event.PLAY:
-                    counter -= 1
+            ### count of games on this day
+            # get the date object for the current date and the previous date
+            date_current = self.get_date(current)
 
-            # also include yesterday's final count
             yesterday = current - datetime.timedelta(days=1)
-            counter += self.datecounts[yesterday]
+            date_previous = self.get_date(yesterday)
 
-            # set today's count
-            self.datecounts[current] = counter
+            # yesterday's count should already be set, so just add today's net
+            date_current.stats["count"] = date_previous.stats["count"] + date_current.net
 
-            # move on to the next date
+            ### update last acquired day if necessary
+            if date_current.acquired:
+                self.last_acquired = current
+
+            ### net change in the last 7 days
+            # get the date object from 7 days ago
+            last_week = current - datetime.timedelta(days=7)
+            date_last_week = self.get_date(last_week)
+
+            # just subtract the counts
+            date_current.stats["net_week"] = date_current.stats["count"] - date_last_week.stats["count"]
+
+            ##########
+            # at the end of the loop, increment the damn date
+            ##########
             current += datetime.timedelta(days=1)
 
     def count(self, date):
         """Count how many games we have on any given date"""
-        return self.datecounts[date]
+        return self.get_date(date).stats["count"]
+
+    def net(self, date):
+        """Net change in games on a given date"""
+        return self.get_date(date).net
+
+    def net_week(self, date):
+        """Net change in games in the week leading up to a given date"""
+        return self.get_date(date).stats["net_week"]
 
     def games_get(self, date):
         """List which games we got on a given date"""
-        return [g for (g,e) in self.datestore[date] if e == Event.GET]
+        return self.get_date(date).games_get()
 
     def games_play(self, date):
         """List which games we played on a given date"""
-        return [g for (g,e) in self.datestore[date] if e == Event.PLAY]
+        return self.get_date(date).games_play()
 
     def last_acquired_date(self):
         """Return the date on which we last acquired a game"""
@@ -160,6 +232,15 @@ class Collection(object):
 
     def tooltip(self, date):
         """Generate the line chart tooltip for a given date (using HTML)"""
+
+        def delta_prefix(val):
+            """Given a value representing a change, return it as a string
+            prefixed by + or - if it's nonzero"""
+
+            if val > 0:
+                return "+" + str(val)
+            else:
+                return str(val)
 
         # let's make a list of lines, and then separate by <br> because html
         lines = []
@@ -172,9 +253,16 @@ class Collection(object):
         datestr = "<b>%s</b>" % date.strftime('%b %d, %Y')
         lines.append(datestr)
 
-        # get the game count
+        # get the game count (and maybe the net, if relevant)
         countstr = "Game Count: <b>%s</b>" % self.count(date)
+        net = self.net(date)
+        if net != 0:
+            countstr += " (%s)" % delta_prefix(net)
         lines.append(countstr)
+
+        # get the rolling 7-day net
+        netweek = "7-Day Rolling Net: <b>%s</b>" % delta_prefix(self.net_week(date))
+        lines.append(netweek)
 
         # put it all together
         multiline = "<br>".join(lines)
